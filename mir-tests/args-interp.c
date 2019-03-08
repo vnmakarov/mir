dcad05f9e00ee1102b5cbc2c664574061b6f68bd
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-03-08 12:19:16 -0500

    Add missed args tests.

---
+++ mir-tests/args-interp.c
@@ -0,0 +1,40 @@
+#include <inttypes.h>
+#include "../mir-interp.h"
+#include "scan-args.h"
+
+static void pri (int64_t c) { printf ("%"PRIx64 "\n", c); }
+static void prf (float f) { printf ("%f\n", f); }
+static void prd (double d) { printf ("%f\n", d); }
+
+int main (void) {
+  MIR_module_t m;
+  MIR_item_t func;
+  MIR_val_t v[19];
+    
+  MIR_init ();
+  MIR_load_external ("pri", pri); MIR_load_external ("prf", prf); MIR_load_external ("prd", prd);
+  m = create_args_module ();
+  func = DLIST_TAIL (MIR_item_t, m->items);
+  MIR_simplify_func (func);
+  MIR_load_module (m);
+  MIR_link ();
+  MIR_interp_init ();
+#if MIR_C_INTERFACE
+  typedef void (*arg_func) (int8_t, int16_t, int32_t, int64_t, float, double,
+			    uint32_t, uint8_t, uint16_t, int32_t, int64_t,
+			    float, float, float, float, float, float, float, double);
+  MIR_set_C_interp_interface (func);
+  ((arg_func) func->addr) (0x01, 0x0002, 0x00000003, 0x100000004, 1.0, 2.0,
+			   0x00000005, 0x06, 0x0007, 0x00000008, 0x100000009,
+			   3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0);
+#else
+  v[0].i = 0x01; v[1].i = 0x0002; v[2].i = 0x00000003; v[3].i = 0x100000004; v[4].f = 1.0; v[5].d = 2.0;
+  v[6].i = 0x00000005; v[7].i = 0x06; v[8].i = 0x0007; v[9].i = 0x00000008; v[10].i = 0x200000009;
+  v[11].f = 3.0; v[12].f = 4.0; v[13].f = 5.0; v[14].f = 6.0; v[15].f = 7.0; v[16].f = 8.0; v[17].f = 9.0;
+  v[18].d = 10.0;
+  v[0] = MIR_interp_arr (func, 19, v);
+#endif
+  MIR_interp_finish ();
+  MIR_finish ();
+  return 0;
+}
---
+++ mir-tests/scan-args.h
@@ -0,0 +1,34 @@
+MIR_module_t create_args_module (void) {
+  const char *str = "\n\
+m:     module\n\
+p_pri: proto v, i64\n\
+p_prf: proto v, f\n\
+p_prd: proto v, d\n\
+       import pri, prf, prd\n\
+       export f\n\
+f:     func i64, 0, i8:i1, i16:i2, i32:i3, i64:i4, f:f1, d:d1, u32:i5, u8:i6, u16:i7, i32:i8, i64:i9, f:f2, f:f3, f:f4, f:f5, f:f6, f:f7, f:f8, d:d2\n\
+       call p_pri, pri, i1\n\
+       call p_pri, pri, i2\n\
+       call p_pri, pri, i3\n\
+       call p_pri, pri, i4\n\
+       call p_pri, pri, i5\n\
+       call p_pri, pri, i6\n\
+       call p_pri, pri, i7\n\
+       call p_pri, pri, i8\n\
+       call p_pri, pri, i9\n\
+       call p_prf, prf, f1\n\
+       call p_prf, prf, f2\n\
+       call p_prf, prf, f3\n\
+       call p_prf, prf, f4\n\
+       call p_prf, prf, f6\n\
+       call p_prf, prf, f7\n\
+       call p_prf, prf, f8\n\
+       call p_prd, prd, d1\n\
+       call p_prd, prd, d2\n\
+       ret 0\n\
+       endfunc\n\
+       endmodule\n\
+";
+  MIR_scan_string (str);
+  return DLIST_TAIL (MIR_module_t, MIR_modules);
+}
