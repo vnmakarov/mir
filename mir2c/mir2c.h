7b0823aa70c3a25dbb5c4a8c8cdf787c580d8550
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-04-14 15:02:24 -0400

    Add missed mir2c.h.

---
+++ mir2c/mir2c.h
@@ -0,0 +1,7 @@
+/* This file is a part of MIR project.
+   Copyright (C) 2018, 2019 Vladimir Makarov <vmakarov.gcc@gmail.com>.
+*/
+
+#include "mir.h"
+
+extern void MIR_module2c (FILE *f, MIR_module_t m);
