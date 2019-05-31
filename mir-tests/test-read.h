b5eb19dfbd8b894a0913cf8ba127c4da7a50d176
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-05-31 17:02:27 -0400

    Split interp-test.  Add new test test8.mir.  Split test-read.h from test-gen.c. Use it in new file test-run.c.

---
+++ mir-tests/test-read.h
@@ -0,0 +1,20 @@
+static char *read_file (const char *name) {
+  FILE *f;
+  size_t len;
+  char *str;
+  
+  if ((f = fopen (name, "r")) == NULL) {
+    perror (name);
+    exit (1);
+  }
+  fseek (f, 0, SEEK_END);
+  len = ftell (f);
+  rewind (f);
+  str = (char *) malloc (len + 1);
+  if (fread (str, 1, len, f) != len) {
+    fprintf (stderr, "file %s was changed\n", name);
+    exit (1);
+  }
+  str[len] = 0;
+  return str;
+}
