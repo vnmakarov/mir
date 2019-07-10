28d53c089c3c59dbc55c86e867a61b9c6ebf4ab9
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-07-10 10:35:54 -0400

    Add some GCC predefined macros.

--- include/mirc/x86-64/mirc-x86_64-linux.h
+++ include/mirc/x86-64/mirc-x86_64-linux.h
@@ -16,6 +16,27 @@
 #define __SIZEOF_SHORT__ 2
 #define __SIZEOF_SIZE_T__ 8
 
+/* Some GCC predefined macros: */
+#define __INT8_MAX__      127
+#define __INT16_MAX__     32767
+#define __INT32_MAX__     2147483647
+#define __INT64_MAX__     9223372036854775807l
+#define __UINT8_MAX__     (__INT8_MAX__ * 2u + 1u)
+#define __UINT16_MAX__    (__INT16_MAX__ * 2u + 1u)
+#define __UINT32_MAX__    (__INT32_MAX__ * 2u + 1u)
+#define __UINT64_MAX__    (__INT64_MAX__ * 2u + 1u)
+#define __SCHAR_MAX__     __INT8_MAX__
+#define __SHRT_MAX__      __INT16_MAX__
+#define __INT_MAX__       __INT32_MAX__
+#define __LONG_MAX__      __INT64_MAX__
+#define __LONG_LONG_MAX__ __INT64_MAX__
+#define __SIZE_MAX__      __UINT64_MAX__
+#define __PTRDIFF_MAX__   __INT64_MAX__
+#define __INTMAX_MAX__    __INT64_MAX__
+#define __UINTMAX_MAX__   __UINT64_MAX__
+#define __INTPTR_MAX__    __INT64_MAX__
+#define __UINTPTR_MAX__   __UINT64_MAX__
+
 typedef unsigned short char16_t;
 typedef unsigned int char32_t;
 
