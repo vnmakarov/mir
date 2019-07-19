33a2d77ef3d792b1a867751547c084d213d81fbf
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-07-19 12:09:57 -0400

    Generate code for access of top-scope identifier.

--- c2mir/c2mir.c
+++ c2mir/c2mir.c
@@ -8226,9 +8226,13 @@ static op_t gen (node_t r, MIR_label_t true_label, MIR_label_t false_label, int 
     e = r->attr;
     if (push_const_val (r, &res)) {
     } else if (e->lvalue_node == NULL) {
-      assert (e->def_node != NULL);
       res = new_op (NULL, MIR_new_ref_op (((decl_t) e->def_node->attr)->item));
-    } else if (! (decl = e->lvalue_node->attr)->reg_p) {
+    } else if ((decl = e->lvalue_node->attr)->scope == top_scope) {
+      t = get_mir_type (e->type);
+      res = get_new_temp (MIR_T_I64);
+      emit2 (MIR_MOV, res.mir_op, MIR_new_ref_op (decl->item));
+      res = new_op (decl, MIR_new_mem_op (t, 0, res.mir_op.u.reg, 0, 1));
+    } else if (! decl->reg_p) {
       t = get_mir_type (e->type);
       res = new_op (decl, MIR_new_mem_op (t, decl->offset, MIR_reg (FP_NAME, curr_func->u.func), 0, 1));
     } else {
