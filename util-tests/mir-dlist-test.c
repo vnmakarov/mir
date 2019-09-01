47c8e44aa99102de1bfc3fb6a81846a2a937ca92
Vladimir N. Makarov <vmakarov@redhat.com>
2019-09-01 10:44:27 -0400

    Format according to clang-format.

--- util-tests/mir-dlist-test.c
+++ util-tests/mir-dlist-test.c
@@ -13,16 +13,21 @@ DEF_DLIST (elem_t, link);
 
 int main (void) {
   int status;
-  DLIST(elem_t) list;
+  DLIST (elem_t) list;
   struct elem e1, e2, e3, e4;
 
-  e1.v = 1; e2.v = 2; e3.v = 3; e4.v = 4;
+  e1.v = 1;
+  e2.v = 2;
+  e3.v = 3;
+  e4.v = 4;
   DLIST_INIT (elem_t, list);
   status = DLIST_LENGTH (elem_t, list) == 0;
   status &= DLIST_HEAD (elem_t, list) == NULL && DLIST_TAIL (elem_t, list) == NULL;
 
-  DLIST_APPEND (elem_t, list, &e3); DLIST_APPEND (elem_t, list, &e4);
-  DLIST_PREPEND (elem_t, list, &e2); DLIST_PREPEND (elem_t, list, &e1);
+  DLIST_APPEND (elem_t, list, &e3);
+  DLIST_APPEND (elem_t, list, &e4);
+  DLIST_PREPEND (elem_t, list, &e2);
+  DLIST_PREPEND (elem_t, list, &e1);
   status &= DLIST_LENGTH (elem_t, list) == 4;
   status &= DLIST_HEAD (elem_t, list) == &e1 && DLIST_TAIL (elem_t, list) == &e4;
   status &= DLIST_NEXT (elem_t, &e1) == &e2 && DLIST_PREV (elem_t, &e4) == &e3;
--- util-tests/mir-varr-test.c
+++ util-tests/mir-varr-test.c
@@ -3,24 +3,31 @@
 DEF_VARR (int);
 int main (void) {
   int status;
-  VARR (int) *test;
+  VARR (int) * test;
   int arr[] = {1, 2, 3};
-  
-  VARR_CREATE (int, test, 0); status = VARR_LENGTH (int, test) == 0;
-  VARR_PUSH (int, test, 42); status &= VARR_LAST (int, test) == 42;
-  VARR_PUSH (int, test, 8); status &= VARR_LAST (int, test) == 8;
-  status &= VARR_SET (int, test, 1, 7) == 8; status &= VARR_GET (int, test, 1) == 7;
-  VARR_EXPAND (int, test, 10); status &= VARR_LENGTH (int, test) == 2;
+
+  VARR_CREATE (int, test, 0);
+  status = VARR_LENGTH (int, test) == 0;
+  VARR_PUSH (int, test, 42);
+  status &= VARR_LAST (int, test) == 42;
+  VARR_PUSH (int, test, 8);
+  status &= VARR_LAST (int, test) == 8;
+  status &= VARR_SET (int, test, 1, 7) == 8;
+  status &= VARR_GET (int, test, 1) == 7;
+  VARR_EXPAND (int, test, 10);
+  status &= VARR_LENGTH (int, test) == 2;
   status &= VARR_ADDR (int, test)[0] == 42 && VARR_ADDR (int, test)[1] == 7;
   VARR_PUSH_ARR (int, test, arr, 3);
   VARR_PUSH (int, test, 4);
   status &= (VARR_ADDR (int, test)[0] == 42 && VARR_ADDR (int, test)[1] == 7
-	     && VARR_ADDR (int, test)[2] == 1 && VARR_ADDR (int, test)[3] == 2
-	     && VARR_ADDR (int, test)[4] == 3 && VARR_ADDR (int, test)[5] == 4
-	     && VARR_LENGTH (int, test) == 6);
-  VARR_TRUNC (int, test, 1); status &= VARR_LENGTH (int, test) == 1;
+             && VARR_ADDR (int, test)[2] == 1 && VARR_ADDR (int, test)[3] == 2
+             && VARR_ADDR (int, test)[4] == 3 && VARR_ADDR (int, test)[5] == 4
+             && VARR_LENGTH (int, test) == 6);
+  VARR_TRUNC (int, test, 1);
+  status &= VARR_LENGTH (int, test) == 1;
   status &= VARR_POP (int, test) == 42;
-  VARR_TRUNC (int, test, 0); VARR_TAILOR (int, test, 10);
+  VARR_TRUNC (int, test, 0);
+  VARR_TAILOR (int, test, 10);
   status &= VARR_LENGTH (int, test) == 10;
   VARR_PUSH (int, test, 42);
   status &= VARR_ADDR (int, test)[10] == 42;
