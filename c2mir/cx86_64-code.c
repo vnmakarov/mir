0edfa91d73df6fafaa0a72c818e358e55edca9dd
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-09-03 16:30:49 -0400

    Format according to .clang-format.

--- c2mir/cx86_64-code.c
+++ c2mir/cx86_64-code.c
@@ -2,15 +2,10 @@
    Copyright (C) 2018, 2019 Vladimir Makarov <vmakarov.gcc@gmail.com>.
 */
 
-static const char *standard_includes[] = {
-  "include/mirc/mirc.h",
-  "include/mirc/x86-64/mirc-x86_64-linux.h"
-};
+static const char *standard_includes[]
+  = {"include/mirc/mirc.h", "include/mirc/x86-64/mirc-x86_64-linux.h"};
 
-static const char *standard_include_dirs[] = {
-  "include/mirc/",
-  "include/mirc/x86-64/"
-};
+static const char *standard_include_dirs[] = {"include/mirc/", "include/mirc/x86-64/"};
 
 #define MAX_ALIGNMENT 16
 
@@ -18,8 +13,7 @@ static const char *standard_include_dirs[] = {
 
 static int x86_adjust_type_alignment (int align, struct type *type) {
   /* see https://www.uclibc.org/docs/psABI-x86_64.pdf */
-  if (type->mode != TM_ARR || raw_type_size (type) < 16)
-    return align;
+  if (type->mode != TM_ARR || raw_type_size (type) < 16) return align;
   return 16;
 }
 
