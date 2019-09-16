850216f8f0c78a3b8961984c2e132cc3d385a131
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-09-16 09:53:19 -0400

    Add MIR_output_module prototype.

--- mir.h
+++ mir.h
@@ -467,6 +467,7 @@ extern const char *MIR_type_str (MIR_context_t ctx, MIR_type_t tp);
 extern void MIR_output_op (MIR_context_t ctx, FILE *f, MIR_op_t op, MIR_func_t func);
 extern void MIR_output_insn (MIR_context_t ctx, FILE *f, MIR_insn_t insn, MIR_func_t func,
                              int newline_p);
+extern void MIR_output_module (MIR_context_t ctx, FILE *f, MIR_module_t module);
 extern void MIR_output (MIR_context_t ctx, FILE *f);
 
 extern void MIR_simplify_func (MIR_context_t ctx, MIR_item_t func, int mem_float_p);
