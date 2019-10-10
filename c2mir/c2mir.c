69e415a1b99e2728a1d4de89173c88893b8296b3
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-10-10 09:18:28 -0400

    Use ref_op in N_ID gen for static linkage too.

--- c2mir/c2mir.c
+++ c2mir/c2mir.c
@@ -10092,7 +10092,7 @@ static op_t gen (node_t r, MIR_label_t true_label, MIR_label_t false_label, int 
     if (e->lvalue_node == NULL) {
       res = new_op (NULL, MIR_new_ref_op (ctx, ((decl_t) e->def_node->attr)->item));
     } else if ((decl = e->lvalue_node->attr)->scope == top_scope || decl->decl_spec.static_p
-               || decl->decl_spec.linkage == N_EXTERN) {
+               || decl->decl_spec.linkage != N_IGNORE) {
       t = get_mir_type (e->type);
       res = get_new_temp (MIR_T_I64);
       emit2 (MIR_MOV, res.mir_op, MIR_new_ref_op (ctx, decl->item));
