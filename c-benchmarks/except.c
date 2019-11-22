de4ea06604cf11536cc6ab5fa825a8885563dc83
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2019-11-22 11:42:18 -0500

    Format by clang-format.

--- c-benchmarks/except.c
+++ c-benchmarks/except.c
@@ -13,40 +13,37 @@ static jmp_buf Hi_exception;
 static jmp_buf Lo_exception;
 
 void blowup (int n) {
-    if (n & 1) {
-    longjmp(Lo_exception, 1);
-    } else {
-    longjmp(Hi_exception, 1);
-    }
+  if (n & 1) {
+    longjmp (Lo_exception, 1);
+  } else {
+    longjmp (Hi_exception, 1);
+  }
 }
 
 void lo_function (volatile int n) {
-    if (setjmp(Lo_exception) != 0) {
+  if (setjmp (Lo_exception) != 0) {
     LO++;
-    } else {
-    blowup(n);
-    }
+  } else {
+    blowup (n);
+  }
 }
 
 void hi_function (volatile int n) {
-    if (setjmp(Hi_exception) != 0) {
+  if (setjmp (Hi_exception) != 0) {
     HI++;
-    } else {
-    lo_function(n);
-    }
+  } else {
+    lo_function (n);
+  }
 }
 
-void some_function (int n) {
-    hi_function(n);
-}
+void some_function (int n) { hi_function (n); }
 
-int
-main(int argc, char *argv[]) {
-    int volatile N = ((argc == 2) ? atoi(argv[1]) : 1);
+int main (int argc, char *argv[]) {
+  int volatile N = ((argc == 2) ? atoi (argv[1]) : 1);
 
-    while (N) {
-    some_function(N--);
-    }
-    printf("Exceptions: HI=%d / LO=%d\n", HI, LO);
-    return(0);
+  while (N) {
+    some_function (N--);
+  }
+  printf ("Exceptions: HI=%d / LO=%d\n", HI, LO);
+  return (0);
 }
--- c-benchmarks/matrix.c
+++ c-benchmarks/matrix.c
@@ -11,58 +11,59 @@
 
 #define SIZE 30
 
-int **mkmatrix(int rows, int cols) {
-    int i, j, count = 1;
-    int **m = (int **) malloc(rows * sizeof(int *));
-    for (i=0; i<rows; i++) {
-    m[i] = (int *) malloc(cols * sizeof(int));
-    for (j=0; j<cols; j++) {
-        m[i][j] = count++;
+int **mkmatrix (int rows, int cols) {
+  int i, j, count = 1;
+  int **m = (int **) malloc (rows * sizeof (int *));
+  for (i = 0; i < rows; i++) {
+    m[i] = (int *) malloc (cols * sizeof (int));
+    for (j = 0; j < cols; j++) {
+      m[i][j] = count++;
     }
-    }
-    return(m);
+  }
+  return (m);
 }
 
-void zeromatrix(int rows, int cols, int **m) {
-    int i, j;
-    for (i=0; i<rows; i++)
-    for (j=0; j<cols; j++)
-        m[i][j] = 0;
+void zeromatrix (int rows, int cols, int **m) {
+  int i, j;
+  for (i = 0; i < rows; i++)
+    for (j = 0; j < cols; j++) m[i][j] = 0;
 }
 
-void freematrix(int rows, int **m) {
-    while (--rows > -1) { free(m[rows]); }
-    free(m);
+void freematrix (int rows, int **m) {
+  while (--rows > -1) {
+    free (m[rows]);
+  }
+  free (m);
 }
 
-int **mmult(int rows, int cols, int **m1, int **m2, int **m3) {
-    int i, j, k, val;
-    for (i=0; i<rows; i++) {
-    for (j=0; j<cols; j++) {
-        val = 0;
-        for (k=0; k<cols; k++) {
+int **mmult (int rows, int cols, int **m1, int **m2, int **m3) {
+  int i, j, k, val;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      val = 0;
+      for (k = 0; k < cols; k++) {
         val += m1[i][k] * m2[k][j];
-        }
-        m3[i][j] = val;
+      }
+      m3[i][j] = val;
     }
-    }
-    return(m3);
+  }
+  return (m3);
 }
 
-int main(int argc, char *argv[]) {
-    int i, n = ((argc == 2) ? atoi(argv[1]) : 1);
-    
-    int **m1 = mkmatrix(SIZE, SIZE);
-    int **m2 = mkmatrix(SIZE, SIZE);
-    int **mm = mkmatrix(SIZE, SIZE);
+int main (int argc, char *argv[]) {
+  int i, n = ((argc == 2) ? atoi (argv[1]) : 1);
 
-    for (i=0; i<n; i++) {
-    mm = mmult(SIZE, SIZE, m1, m2, mm);
-    }
-    printf("%d %d %d %d\n", mm[0][0], mm[2][3], mm[3][2], mm[4][4]);
+  int **m1 = mkmatrix (SIZE, SIZE);
+  int **m2 = mkmatrix (SIZE, SIZE);
+  int **mm = mkmatrix (SIZE, SIZE);
 
-    freematrix(SIZE, m1);
-    freematrix(SIZE, m2);
-    freematrix(SIZE, mm);
-    return(0);
+  for (i = 0; i < n; i++) {
+    mm = mmult (SIZE, SIZE, m1, m2, mm);
+  }
+  printf ("%d %d %d %d\n", mm[0][0], mm[2][3], mm[3][2], mm[4][4]);
+
+  freematrix (SIZE, m1);
+  freematrix (SIZE, m2);
+  freematrix (SIZE, mm);
+  return (0);
 }
