9a8d7d47c89a21a8952ba8f0c2b8892df5fd8254
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2020-01-15 20:55:02 -0500

    Modify va_list for osx.

--- include/mirc/x86-64/stdarg.h
+++ include/mirc/x86-64/stdarg.h
@@ -6,12 +6,16 @@
 #ifndef __STDARG_H
 #define __STDARG_H
 
+#ifdef __APPLE__
+typedef __darwin_va_list va_list;
+#else
 typedef struct {
   unsigned int gp_offset;
   unsigned int fp_offset;
   void *overflow_arg_area;
   void *reg_save_area;
 } va_list[1];
+#endif
 
 #define va_start(ap, param) __builtin_va_start (ap)
 #define va_arg(ap, type) __builtin_va_arg(ap, (type *) 0)
