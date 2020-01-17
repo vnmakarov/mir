a72f5dd7382749148d711a54a7f1df1d4e279fb9
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2020-01-17 12:46:33 -0500

    Remove func1, func2.

--- mir-tests/simplify.c
+++ mir-tests/simplify.c
@@ -5,11 +5,10 @@
 
 int main (void) {
   MIR_module_t m;
-  MIR_item_t func1, func2;
   MIR_context_t ctx = MIR_init ();
 
-  func1 = create_mir_func_with_loop (ctx, &m);
-  func2 = create_mir_example2 (ctx, &m);
+  create_mir_func_with_loop (ctx, &m);
+  create_mir_example2 (ctx, &m);
   MIR_load_module (ctx, m);
   MIR_link (ctx, MIR_set_interp_interface, NULL);
   fprintf (stderr, "Simplified code:\n");
