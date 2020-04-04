f71749a06ebc8415fa936fe4a102534be31b2fdf
Vladimir Makarov <vmakarov.gcc@gmail.com>
2020-04-04 10:19:33 -0700

    Define int8_t when __int8_t_defined is not defined.

--- include/mirc/aarch64/stdint.h
+++ include/mirc/aarch64/stdint.h
@@ -6,7 +6,10 @@
 #ifndef _STDINT_H
 #define _STDINT_H 1
 
-typedef char int8_t;
+#ifndef __int8_t_defined
+#define __int8_t_defined
+typedef signed char int8_t;
+#endif
 typedef short int int16_t;
 typedef int int32_t;
 typedef long int int64_t;
