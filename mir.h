494c45176725b3e1a383e1fe3ffdb2a45d739805
Vladimir N. Makarov <vmakarov@redhat.com>
2020-06-03 11:17:45 -0400

    Include pthread.h.  Comment MIR_NO_PARALLEL_GEN definition.

--- mir.h
+++ mir.h
@@ -31,7 +31,10 @@ static inline int mir_assert (int cond) { return 0 && cond; }
 #define MIR_NO_SCAN 0
 #endif
 
-#define MIR_NO_PARALLEL_GEN 1
+//#define MIR_NO_PARALLEL_GEN 1
+#ifndef MIR_NO_PARALLEL_GEN
+#include <pthread.h>
+#endif
 
 #ifdef __GNUC__
 #define MIR_UNUSED __attribute__ ((unused))
