e67bcf51169c70eb486b6904cd2036779e7b22dd
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2020-07-06 13:48:25 -0400

    Add comment for the cond-cast.c.

--- c-tests/new/cond-cast.c
+++ c-tests/new/cond-cast.c
@@ -1,5 +1,6 @@
 int time_limit;
 void func (int average, int simple_average) {
+  /* Generation for cond parts of different types: */
   time_limit = (average < 2.0 * simple_average) ? average : 2.0 * simple_average;
 }
 
