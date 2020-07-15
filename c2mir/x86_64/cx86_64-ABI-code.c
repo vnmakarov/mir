f37a258e893806f59c987795db62857f10166ec5
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2020-07-15 11:54:17 -0400

    Increase n_iregs when passing aggregate by ref.

--- c2mir/x86_64/cx86_64-ABI-code.c
+++ c2mir/x86_64/cx86_64-ABI-code.c
@@ -126,6 +126,7 @@ static void target_add_res (MIR_context_t ctx, struct func_type *func_type,
     var.type = MIR_POINTER_TYPE;
     VARR_PUSH (MIR_var_t, proto_info.arg_vars, var);
     proto_info.res_ref_p = TRUE;
+    arg_info->n_iregs++;
   }
   var.name = RET_VAL_NAME;
   var.type = get_mir_type (ctx, func_type->ret_type);
