6d77effc54ad471ed50c0b8b0f326c5d2de32aea
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2020-09-03 10:35:28 -0400

    Exchange move operands in target_gen_gather_arg.

--- c2mir/x86_64/cx86_64-ABI-code.c
+++ c2mir/x86_64/cx86_64-ABI-code.c
@@ -411,10 +411,10 @@ static int target_gen_gather_arg (MIR_context_t ctx, const char *name, struct ty
     type = qword_types[i];
     reg_var = get_reg_var (ctx, type, qword_name (ctx, name, i));
     MIR_append_insn (ctx, curr_func,
-                     MIR_new_insn (ctx, tp_mov (type), MIR_new_reg_op (ctx, reg_var.reg),
+                     MIR_new_insn (ctx, tp_mov (type),
                                    MIR_new_mem_op (ctx, type, param_decl->offset + 8 * i,
-                                                   MIR_reg (ctx, FP_NAME, curr_func->u.func), 0,
-                                                   1)));
+                                                   MIR_reg (ctx, FP_NAME, curr_func->u.func), 0, 1),
+                                   MIR_new_reg_op (ctx, reg_var.reg)));
   }
   return TRUE;
 }
