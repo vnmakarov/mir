10471019c3c3f7ec8e04d3b4cfd967352c36a3a2
Vladimir N. Makarov <vmakarov@redhat.com>
2020-11-26 16:53:28 -0500

    Stringify std include files in dir s390x.

--- c2mir/s390x/mirc_s390x_stdarg.h
+++ c2mir/s390x/mirc_s390x_stdarg.h
@@ -2,23 +2,24 @@
    Copyright (C) 2020 Vladimir Makarov <vmakarov.gcc@gmail.com>.
 */
 
-#ifndef __STDARG_H
-#define __STDARG_H
-
-typedef struct {
-  long __gpr, __fpr;
-  void *__overflow_arg_area;
-  void *__reg_save_area;
-} va_list[1];
-
-#define va_start(ap, param) __builtin_va_start (ap)
-#define va_arg(ap, type) __builtin_va_arg(ap, (type *) 0)
-#define va_end(ap) 0
-#define va_copy(dest, src) ((dest) = (src))
-
-/* For standard headers of a GNU system: */
-#ifndef __GNUC_VA_LIST
-#define __GNUC_VA_LIST 1
-#endif
-typedef va_list __gnuc_va_list;
-#endif /* #ifndef __STDARG_H */
+static char stdarg_str[]
+  = "#ifndef __STDARG_H\n"
+    "#define __STDARG_H\n"
+    "\n"
+    "typedef struct {\n"
+    "  long __gpr, __fpr;\n"
+    "  void *__overflow_arg_area;\n"
+    "  void *__reg_save_area;\n"
+    "} va_list[1];\n"
+    "\n"
+    "#define va_start(ap, param) __builtin_va_start (ap)\n"
+    "#define va_arg(ap, type) __builtin_va_arg(ap, (type *) 0)\n"
+    "#define va_end(ap) 0\n"
+    "#define va_copy(dest, src) ((dest) = (src))\n"
+    "\n"
+    "/* For standard headers of a GNU system: */\n"
+    "#ifndef __GNUC_VA_LIST\n"
+    "#define __GNUC_VA_LIST 1\n"
+    "#endif\n"
+    "typedef va_list __gnuc_va_list;\n"
+    "#endif /* #ifndef __STDARG_H */\n";
