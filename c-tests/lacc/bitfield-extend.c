88a8fbc8e86336d2000ddc4c18d4d62160d8f7e7
Vladimir N. Makarov <vmakarov@redhat.com>
2020-12-03 22:20:25 -0500

    Make adjustment for Windows (long and exit code).

--- c-tests/lacc/bitfield-extend.c
+++ c-tests/lacc/bitfield-extend.c
@@ -1,13 +1,13 @@
 struct fields {
-	signed int foo:5;
-	unsigned int bar:1;
+  signed int foo : 5;
+  unsigned int bar : 1;
 };
 
-int main(void) {
-	struct fields test = {0};
+int main (void) {
+  struct fields test = {0};
 
-	test.foo--;
-	test.bar = 1;
+  test.foo--;
+  test.bar = 1;
 
-	return test.foo - test.bar;
+  return (unsigned char) (test.foo - test.bar) != 254;
 }
--- c-tests/lacc/struct.c
+++ c-tests/lacc/struct.c
@@ -1,21 +1,21 @@
 typedef struct {
-	int x;
-	int y;
-	struct {
-		int val;
-	} z;
+  int x;
+  int y;
+  struct {
+    int val;
+  } z;
 } point_t;
 
-int main() {
-	point_t point;
-	point_t *foo;
+int main () {
+  point_t point;
+  point_t *foo;
 
-	point.x = 1;
-	point.y = 3;
+  point.x = 1;
+  point.y = 3;
 
-	foo = &point;
-	foo->x = 17;
-	foo->z.val = 5;
+  foo = &point;
+  foo->x = 17;
+  foo->z.val = 5;
 
-	return point.y * point.z.val - point.x;
+  return (unsigned char) (point.y * point.z.val - point.x) != 254;
 }
