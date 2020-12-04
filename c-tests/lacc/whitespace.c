cae8d79cc140bcb62235a9ad982e3ec524d62dfb
Vladimir N. Makarov <vmakarov@redhat.com>
2020-12-04 11:53:15 -0500

    Modify tests for Windows puts result meaning.

--- c-tests/lacc/whitespace.c
+++ c-tests/lacc/whitespace.c
@@ -1,11 +1,11 @@
-int puts(const char *);
+int puts (const char *);
 void abort (void);
- /* form feed, 0x0c */
+/* form feed, 0x0c */
 
-int main(void) {
-#ifdef __APPLE__
-  return !(puts("Hello") >= 0);
+int main (void) {
+#if defined(__APPLE__) || defined(_WIN32) /* different puts return meaning */
+  return !(puts ("Hello") >= 0);
 #else
-  return !(puts("Hello") == 6);
+  return !(puts ("Hello") == 6);
 #endif
 }
