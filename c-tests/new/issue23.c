88eac29f51c37aff6f2efbeb8dbd6b2203380692
Vladimir N. Makarov <vmakarov@redhat.com>
2020-12-10 16:39:04 -0500

    Modify issue23 case to work with Windows too.

--- c-tests/new/issue23.c
+++ c-tests/new/issue23.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 int main (void) {
-  printf ("%lf\n", NAN);
-  printf ("%lf\n", INFINITY);
-  printf ("%lf\n", HUGE_VAL);
-  printf ("%f\n", HUGE_VALF);
-  printf ("%Lf\n", HUGE_VALL);
-  return 0;
+  char buf[1000], n;
+  n = sprintf (buf, "%lf", NAN);
+  n += sprintf (&buf[n], ";%lf", INFINITY);
+  n += sprintf (&buf[n], ";%lf", HUGE_VAL);
+  n += sprintf (&buf[n], ";%f", HUGE_VALF);
+  n += sprintf (&buf[n], ";%Lf", HUGE_VALL);
+#ifdef _WIN32
+  return strcmp (buf, "-nan(ind);inf;inf;inf;inf") != 0;
+#else
+  return strcmp (buf, "nan;inf;inf;inf;inf") != 0;
+#endif
 }
