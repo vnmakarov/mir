e092427e3467c0398f0ff0e442a6165d019e6eb6
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2021-05-04 13:35:01 -0400

    Add test for the ssa rename bug.

---
+++ c-tests/new/ssa.c
@@ -0,0 +1,14 @@
+int a, c;
+int *b = &a;
+void d (int e, ...) {
+  *b = e;
+  for (; 0;)
+    for (;; e++)
+      for (; 0;)
+        ;
+}
+int main (void) {
+  d (1, c);
+  printf ("%d\n", a);
+  return 0;
+}
