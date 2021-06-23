08763682d2f9243b715f5880507934fb31b7fde8
Vladimir Makarov <vmakarov.gcc@redhat.com>
2021-06-23 10:41:29 -0400

    Check __risc_flen too.

--- mir.c
+++ mir.c
@@ -6009,9 +6009,12 @@ void _MIR_dump_code (const char *name, int index, uint8_t *code, size_t code_len
 #elif defined(__s390x__)
 #include "mir-s390x.c"
 #elif defined(__riscv)
-#if __riscv_xlen != 64 || !__riscv_float_abi_double || !__riscv_mul || !__riscv_div \
-  || !__riscv_compressed
+#if __riscv_xlen != 64 || __riscv_flen < 64 || !__riscv_float_abi_double || !__riscv_mul \
+  || !__riscv_div || !__riscv_compressed
 #error "only 64-bit RISCV supported (at least rv64imafdc)"
+#endif
+#if __riscv_flen == 128
+#error "RISCV 128-bit floats (Q set) is not supported"
 #endif
 #include "mir-riscv64.c"
 #else
