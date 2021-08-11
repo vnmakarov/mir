3081a56e6b360565e8a9bcff1b4dde8aa6b36068
Vladimir N. Makarov <vmakarov@redhat.com>
2021-08-11 12:06:35 -0400

    Make line-directive.c test working independently of current directory.

--- c-tests/lacc/line-directive.c
+++ c-tests/lacc/line-directive.c
@@ -1,12 +1,8 @@
-int printf(const char *, ...);
+int printf (const char *, ...);
 
 #line __LINE__
 int i = __LINE__;
-char *s = __FILE__;
-# line 10 "bar.c"
+#line 10 "bar.c"
 int j = __LINE__;
 
-int main(void) {
-	return printf("%d, %d, %d, %s, %s\n",
-		__LINE__, i, j, s, __FILE__);
-}
+int main (void) { return printf ("%d, %d, %d, %s\n", __LINE__, i, j, __FILE__); }
