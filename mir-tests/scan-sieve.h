c8049f51bd47d67349c4779a167b708721f7ec45
Vladimir N. Makarov <vmakarov@redhat.com>
2021-11-04 14:24:56 -0400

    Add different aliases for memory.

--- mir-tests/scan-sieve.h
+++ mir-tests/scan-sieve.h
@@ -25,15 +25,15 @@ sieve:   func i64\n\
 loop:    bge fin, iter, " ITER
       "\n\
          mov count, 0;  mov i, 0\n\
-loop2:   mov u8:(flags, i), 1;  add i, i, 1\n\
+loop2:   mov u8:(flags, i)::n1, 1;  add i, i, 1\n\
          blt loop2, i, " FLAGS
       "\n\
          mov i, 2\n\
-loop3:   beq cont3, u8:(flags,i), 0\n\
+loop3:   beq cont3, u8:(flags,i):c, 0\n\
          add prime, i, 1; add k, i, prime\n\
 loop4:   bge fin4, k, " FLAGS
       "\n\
-         mov u8:(flags, k), 0;  add k, k, prime\n\
+         mov u8:(flags, k):c:n2, 0;  add k, k, prime\n\
          jmp loop4\n\
 fin4:    add count, count, 1\n\
 cont3:   add i, i, 1\n\
