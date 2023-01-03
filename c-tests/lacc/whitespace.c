bc81e2f2b7fa994ff25875adfa077f13aef4a20e
Vladimir Makarov <vmakarov.gcc@gmail.com>
2023-01-03 15:04:58 +0000

    Define puts as printf in lacc tests, change checks of puts exit code for apple and win32.

--- c-tests/lacc/whitespace.c
+++ c-tests/lacc/whitespace.c
@@ -1,11 +1,7 @@
 int puts (const char *);
 void abort (void);
 /* form feed, 0x0c */
-
+#define puts(s) printf ("%s\n", s)
 int main (void) {
-#if defined(__APPLE__) || defined(_WIN32) /* different puts return meaning */
-  return !(puts ("Hello") >= 0);
-#else
   return !(puts ("Hello") == 6);
-#endif
 }
