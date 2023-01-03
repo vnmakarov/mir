98bbd53ed666f80b75c50fb524b3b537ef3299de
Vladimir Makarov <vmakarov.gcc@gmail.com>
2023-01-03 15:54:52 -0500

    M1 Apple: add the right prototype for printf in c-tests.

--- c-tests/lacc/whitespace.c
+++ c-tests/lacc/whitespace.c
@@ -1,7 +1,6 @@
 int puts (const char *);
 void abort (void);
 /* form feed, 0x0c */
+extern int printf (const char *, ...);
 #define puts(s) printf ("%s\n", s)
-int main (void) {
-  return !(puts ("Hello") == 6);
-}
+int main (void) { return !(puts ("Hello") == 6); }
